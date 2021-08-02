943c8ed3b39049cbfca8a5da1635850cce133bdb
shoshlavosh <shoshlavosh@gmail.com>
2021-08-02 19:55:07 +0000

    partially completed lab

--- trials.py
+++ trials.py
@@ -2,36 +2,79 @@
 
 
 def output_all_items(items):
-    pass  # TODO: replace this line with your code
 
+    for item in items:
+        print(item)
 
 def get_all_evens(nums):
-    pass  # TODO: replace this line with your code
 
+    even_nums = []
+    for num in nums:
+        if num % 2 == 0:
+            even_nums.append(num)
+
+    return even_nums
 
 def get_odd_indices(items):
-    pass  # TODO: replace this line with your code
+
+
+    result = []
+
+    for idx in items:
+        if idx % 2 != 0:
+            result.append(items[idx])
+
+    return result 
+
 
 
 def print_as_numbered_list(items):
-    pass  # TODO: replace this line with your code
+  
+
+    i = 1 
+    for item in items:
+        print(i, item)
+        i += 1
 
 
 def get_range(start, stop):
-    pass  # TODO: replace this line with your code
+ 
+
+    nums = []
+    for i in range(start, stop):
+        nums.append(i)
+
+    return nums
 
 
 def censor_vowels(word):
-    pass  # TODO: replace this line with your code
+
+    chars = []
+    vowels = "aeiou"
+
+    for letter in word:
+        if letter in vowels:
+            chars.append("*")
+        else:
+            chars.append(letter)
+
+    return "".join(chars)
 
 
 def snake_to_camel(string):
-    pass  # TODO: replace this line with your code
 
+
+    
 
 def longest_word_length(words):
-    pass  # TODO: replace this line with your code
+    
+    longest = len(words[0])
+    for word in words: 
+        if longest < len(word):
+            longest == len(word)
 
+    return longest
+            
 
 def truncate(string):
     pass  # TODO: replace this line with your code
